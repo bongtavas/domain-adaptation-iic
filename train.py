ee1b02a460f573affc90868a3454ee86be083ae8
Romelio P. Tavas Jr <romeliotavas@gmail.com>
2019-12-14 14:53:44 +0800

    Use two head model

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from models.net5g import ClusterNet5g
+from models.net5g_two_head import ClusterNet5gTwoHead
 from dataset import MNIST_SVHN
 from torch.utils.data import DataLoader
 from utils.IID_losses import IID_loss
@@ -37,6 +37,11 @@ def parse_config():
     parser.add_argument("--results_path", type=str, default="results")
     parser.add_argument("--save_freq", type=int, default=10)
 
+    # Model Settings
+    parser.add_argument("--head_A_first", default=True, action="store_true")
+    parser.add_argument("--head_A_epochs", type=int, default=1)
+    parser.add_argument("--head_B_epochs", type=int, default=2)
+
     parser.add_argument("--output_k_A", type=int, default=50)
     parser.add_argument("--output_k_B", type=int, default=10)
 
@@ -61,65 +66,81 @@ def train(config):
 
     train_loader = DataLoader(dataset, batch_size=config.batch_sz, shuffle=True)
 
-    net_model = ClusterNet5g(config)
+    model = ClusterNet5gTwoHead(config)
 
-    net_model.cuda()
+    model.cuda()
 
-    net_model = torch.nn.DataParallel(net_model)
+    model = torch.nn.DataParallel(model)
 
-    optimiser = get_opt(config.opt)(net_model.module.parameters(), lr=config.lr)
+    optimiser = get_opt(config.opt)(model.module.parameters(), lr=config.lr)
 
     best_acc = 0.
 
     model_save_path = os.path.join(config.results_path, "iic-adapt.pt")
 
+    heads = ["A", "B"]
+
+    head_epochs = {}
+    head_epochs["A"] = config.head_A_epochs
+    head_epochs["B"] = config.head_B_epochs
+
     for epoch in range(1, config.num_epochs + 1):
-        start = time.time()
+        start_time = time.time()
 
-        # if epoch in config.lr_schedule:
-        #     optimiser = update_lr(optimiser, lr_mult=config.lr_mult)
+        for head_i in range(2):
+            head = heads[head_i]
+            for head_i_epoch in range(head_epochs[head]):
+                for batch_i, batch in enumerate(train_loader):
+                    train_model(model, head, batch, batch_i, epoch, optimiser, start_time)
+        stats = evaluate(model, config)
+        print_stats(stats)
+        if stats["best"] > best_acc:
+            best_acc = stats["best"]
+            logging.info("Accuracy improved, saving model")
+            torch.save(model.state_dict, model_save_path)
 
-        for batch_i, batch in enumerate(train_loader):
-            net_model.module.zero_grad()
-            mnist_x, svhn_x, label_y = batch
-            mnist_x = mnist_x.cuda()
-            svhn_x = svhn_x.cuda()
-            label_y = label_y.cuda()
 
-            mnist_x = sobel_process(mnist_x, config.include_rgb)
-            svhn_x = sobel_process(svhn_x, config.include_rgb)
+def train_model(model, head, batch, batch_i, epoch, optimiser, start_time):
+    model.module.zero_grad()
+    mnist_x, svhn_x, label_y = batch
+    mnist_x = mnist_x.cuda()
+    svhn_x = svhn_x.cuda()
+    label_y = label_y.cuda()
 
-            z = net_model(mnist_x)
-            z_prime = net_model(svhn_x)
+    mnist_x = sobel_process(mnist_x, config.include_rgb)
+    svhn_x = sobel_process(svhn_x, config.include_rgb)
 
-            avg_loss_batch = None  # avg over the heads
-            avg_loss_no_lamb_batch = None
-            for i in range(config.num_sub_heads):
-                loss, loss_no_lamb = IID_loss(z[i], z_prime[i], config.lamb)
-                if avg_loss_batch is None:
-                    avg_loss_batch = loss
-                    avg_loss_no_lamb_batch = loss_no_lamb
-                else:
-                    avg_loss_batch += loss
-                    avg_loss_no_lamb_batch += loss_no_lamb
+    z = model(mnist_x, head=head)
+    z_prime = model(svhn_x, head=head)
 
-            avg_loss_batch /= config.num_sub_heads
-            avg_loss_no_lamb_batch /= config.num_sub_heads
-            end = time.time()
+    avg_loss_batch = None  # avg over the heads
+    avg_loss_no_lamb_batch = None
+    for i in range(config.num_sub_heads):
+        loss, loss_no_lamb = IID_loss(z[i], z_prime[i], config.lamb)
+        if avg_loss_batch is None:
+            avg_loss_batch = loss
+            avg_loss_no_lamb_batch = loss_no_lamb
+        else:
+            avg_loss_batch += loss
+            avg_loss_no_lamb_batch += loss_no_lamb
 
-            if batch_i % 40 == 0:
-                logging.info("=== Epoch {%s} Batch: {%d} Avg Loss: {%f} Avg Loss No Lamb: {%f}  Running time: {%4f}" % (str(epoch), batch_i, avg_loss_batch.item(), avg_loss_no_lamb_batch.item(), end - start))
+    avg_loss_batch /= config.num_sub_heads
+    avg_loss_no_lamb_batch /= config.num_sub_heads
+    end = time.time()
 
-            avg_loss_batch.backward()
-            optimiser.step()
+    if batch_i % 40 == 0:
+        logging.info("=== Epoch {%s} Head {%s} Batch: {%d} Avg Loss: {%f} Avg Loss No Lamb: {%f}  Running time: {%4f}" %
+                     (str(epoch), head, batch_i, avg_loss_batch.item(), avg_loss_no_lamb_batch.item(), end - start_time))
 
-        if epoch % 1 == 0:
-            stats = evaluate(net_model, config)
-            logging.info(str(stats))
-            if stats["best"] > best_acc:
-                best_acc = stats["best"]
-                logging.info("Accuracy improved, saving model")
-                torch.save(net_model.state_dict, model_save_path)
+    avg_loss_batch.backward()
+    optimiser.step()
+
+
+def print_stats(stats):
+    print("Stats:")
+    for key in stats:
+        print("{} : {}".format(key, stats[key]))
+    print()
 
 
 def evaluate(net_model, config):
